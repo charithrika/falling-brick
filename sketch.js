242740247e26b881833d82c80b9767b0ac02c46c
charithrika <98090414+charithrika@users.noreply.github.com>
2022-01-20 16:34:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,27 @@
-var box;
+//2. Create a variable named brick at the first line of "sketch.js"
+var brick;
 function setup()
 {
- createCanvas(400,400)
- box=createSprite(40,40,40,40)
- box.shapeColor= "green";
- box.velocityX=5;
- box.rotation=50;
- box.x=200;
- box.y=200;
+ createCanvas(400,400);
+ //3. Create a brick sprite by assigning brick variable to createSprite(200,0,100,40)
+ brick=createSprite(200,0,100,40);
+
+ //5. set color of the brick sprite to brown
+ brick.shapeColor="brown";
+
+ //6. Give vertical movement to the brick by setting the brick's y velocity to 0.5
+ brick.velocity.y=0.5;
+ 
 
 }
 
 function draw()
 {
- background(220);
+ //1.set background of canvas to 'lightblue' inside the draw function
+ background('lightblue');
+
+
+ //4.Display the brick sprite by Calling the drawSprites() function inside the draw() function;
  drawSprites();
 }
 
